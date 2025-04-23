96d5cc40cc91acbe5987585f8df00357e42209ca
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-04-23 15:22:18 +0530

    header-strip

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='text-3xl bg-red-400'>Konichiwa</div>
+      <Header />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ code/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-around items-center bg-green-400 text-black py-2">
+        <span> CBSE Affiliation No:1130804</span>
+        <span>School U-DISE No: 27090501510 (CBSE) </span>
+        <span>+91-9823084786, 7028997786 </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
