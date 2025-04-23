a5c0d21993b242290f59ee993535098d0a3d2330
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-04-23 16:21:50 +0530

    Header Strip Completed

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import HeaderStrip from "./HeaderStrip";
 
 const Header = () => {
+
+
   return (
-    <header>
-      <div className="flex justify-around items-center bg-green-400 text-black py-2">
-        <span> CBSE Affiliation No:1130804</span>
-        <span>School U-DISE No: 27090501510 (CBSE) </span>
-        <span>+91-9823084786, 7028997786 </span>
+    <header className="max-w-screen w-full mx-auto bg-white shadow-md sticky top-0 z-50">
+      <HeaderStrip/>
+      <div>
+        
       </div>
     </header>
   );
---
+++ code/src/components/HeaderStrip.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Logo from '../assets/logo.png';
+
+const HeaderStrip = () => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 780) {
+        setShow(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    // Initial check
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div
+      style={{ display: show ? "flex" : "none" }}
+      className="flex justify-between max-w-screen w-full gap-30 items-center bg-shade-blue text-black py-2"
+    >
+      <div className="h-10 pt-1 w-xl xl:max-w-80 pl-10 font-bold text-white uppercase text-xl relative">
+        Indian Olympiad School
+      </div>
+      <div className="flex max-w-screen-md gap-18 w-full text-orange-300 relative text-lg">
+        <span className="flex flex-col items-start text-orange-300 relative partition-line text-[16px] xl:text-lg">
+          <span className="text-white font-semibold text-sm">
+            CBSE Affiliation No:
+          </span>
+          1130804
+        </span>
+        <span className="flex flex-col items-start relative partition-line text-orange-300  text-[16px] xl:text-lg">
+          <span className="text-white font-semibold text-sm">
+            School U-DISE No:{" "}
+          </span>{" "}
+          27090501510 (CBSE)
+        </span>
+        <span className="flex flex-col items-start text-orange-300 text-[16px] xl:text-lg">
+          <span className="text-white font-semibold text-sm">Contact No:</span>
+          9823084786, 7028997786
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderStrip;
